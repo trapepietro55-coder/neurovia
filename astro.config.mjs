aeb30598c895b121785507f2a9fd609f37e41ba3
Pietro Trape <Trapepietro55@gmail.com>
2026-02-06 05:31:45 +0100

    astro output static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 
 export default defineConfig({
-  output: "server",
+  output: "static",
   adapter: node({ mode: "standalone" }),
 });
 
